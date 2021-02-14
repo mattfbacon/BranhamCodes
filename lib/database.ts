d12f45432c07ebe4b2d77ca16d1bf0d338c56d86
Matt Fellenz <mattf53190@gmail.com>
2021-02-14 14:05:39 -0800

    Add logging to database.ts

--- lib/database.ts
+++ lib/database.ts
@@ -21,12 +21,14 @@ class DBManager {
 		await this.db.drop();
 	}
 	private get_user(user_string: string) {
+		db_logger.debug(`get_user: ${user_string}`);
 		return this.db.findOne({ user_string, });
 	}
 	async add_user(username: string, url: string) {
 		const user = await this.db.findOne({ username, });
 		if (user === null) {
 			const user_string = uuid();
+			db_logger.debug(`add_user: ${username} ${url} ${user_string}`);
 			await this.db.insertOne({
 				'avatar_url': url,
 				'problems': [ 1, ],
@@ -35,31 +37,34 @@ class DBManager {
 			});
 			return user_string;
 		}
-		db_logger.warn(`user already exists: ${username}`);
+		db_logger.warn(`in add_user: user already exists: ${username}`);
 		return user.user_string;
 	}
 	async get_user_problems(user_string: string) {
 		const user = await this.get_user(user_string);
 		if (user !== null) {
+			db_logger.debug(`get_user_problems: ${user_string}: ${user.problems}`);
 			return user.problems;
 		}
-		db_logger.warn(`user does not exist: ${user_string}`);
+		db_logger.warn(`in get_user_problems: user does not exist: ${user_string}`);
 		return [ 1, ];
 	}
 	async has_user_problem(user_string: string, problem_num: number) {
 		const user = await this.get_user(user_string);
 		if (user !== null) {
+			db_logger.debug(`has_user_problem: ${user_string} ${problem_num}`);
 			return user.problems.includes(problem_num);
 		}
-		db_logger.warn(`user does not exist: ${user_string}`);
+		db_logger.warn(`in has_user_problem: user does not exist: ${user_string}`);
 		return false;
 	}
 	async add_user_problems(user_string: string, ...problems: number[]) {
 		const user = await this.get_user(user_string);
 		if (user === null) {
-			db_logger.warn(`user does not exist: ${user_string}`);
+			db_logger.warn(`in add_user_problems: user does not exist: ${user_string}`);
 			return false;
 		}
+		db_logger.debug(`add_user_problems: ${user_string} ${problems}`);
 		this.db.updateOne({ '_id': user._id, }, {
 			'$addToSet': {
 				'problems': { '$each': problems, },
