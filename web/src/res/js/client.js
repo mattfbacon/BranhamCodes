c08de11c6eef3b91af40d3a3fbf89c83a5abe05f
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2021-02-14 15:49:58 -0800

    Save problems array not string

--- web/src/res/js/client.js
+++ web/src/res/js/client.js
@@ -3,12 +3,12 @@
 const get_problems = function() {
 	const xmlhttp = new XMLHttpRequest();
 	let string;
-	xmlhttp.open('GET', 'http://localhost:9000/user_problems', true);
+	xmlhttp.open('GET', '/user_problems', true);
 	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
 			string = xmlhttp.responseText;
 			const problems = JSON.parse(string);
-			localStorage.setItem('problems', string);
+			localStorage.setItem('problems', problems);
 		}
 	};
 	xmlhttp.send();
