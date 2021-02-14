2c45c248a857a294022bc963cc544a59ed8bad7a
Matt Fellenz <mattf53190@gmail.com>
2021-02-14 12:51:37 -0800

    add DEBUG and KEY to config

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,15 @@
 'use strict';
 
-export default {
+interface Config {
+	DEBUG: boolean;
+	KEY: string | null;
+	PORT: number;
+}
+
+const config: Config = {
+	'DEBUG': false,
+	'KEY': null,
 	'PORT': 9000,
 };
+
+export default config;
