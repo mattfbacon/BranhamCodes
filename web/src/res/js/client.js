880f98a75e7174ecbc5b6169c3b8a4cea1169787
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2021-02-15 09:26:26 -0800

    async function around whole file

--- web/src/res/js/client.js
+++ web/src/res/js/client.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const get_problems = async function() {
-	const response = await fetch('/user_problems');
-	return response.json();
-};
+(async () => {
+	const get_problems = async function() {
+		const response = await fetch('/user_problems');
+		return response.json();
+	};
 
-if (localStorage.getItem('problems') === null) {
-	(async () => {
+	if (localStorage.getItem('problems') === null) {
 		const problems = await get_problems();
 		localStorage.setItem('problems', problems);
-	})().catch((reason) => {
-		console.log(reason);
-	});
-}
+	}
+})().catch((reason) => {
+	console.log(reason);
+});
