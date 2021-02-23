c9c7ee583d862db9c72303e5ba6c561401feca15
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2021-02-22 18:31:43 -0800

    Use problems from localStorage

--- lib/config.ts
+++ lib/config.ts
@@ -1,16 +1,16 @@
 'use strict';
 
 interface Config {
-	DEBUG: boolean;
-	GITHUB_ID: string;
-	GITHUB_SECRET: string;
-	PORT: number;
+    DEBUG: boolean;
+    GITHUB_ID: string;
+    GITHUB_SECRET: string;
+    PORT: number;
 }
 
 const config: Config = {
-	'DEBUG': false,
-	'GITHUB_ID': null,
-	'GITHUB_SECRET': null,
+	'DEBUG': true,
+	'GITHUB_ID': '94e29cefda7a6c5d20a8',
+	'GITHUB_SECRET': '5cf89f5366e0d8a7bbc9eddbcaa5a8353cf95f14',
 	'PORT': 9000,
 };
 
